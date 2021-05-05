91b592bc5922806d4c7c2e00c9231151b0a12a26
Davis Bray <45185764+Kaltag1925@users.noreply.github.com>
2021-05-05 17:05:15 -0500

    Update 3df864dc6f913f5b@254.js

--- 3df864dc6f913f5b@254.js
+++ 3df864dc6f913f5b@254.js
@@ -1,7 +1,7 @@
 // https://observablehq.com/@kaltag1925/test@254
 export default function define(runtime, observer) {
   const main = runtime.module();
-  const fileAttachments = new Map([["artifacts.json",new URL("",import.meta.url)]]);
+  const fileAttachments = new Map([["artifacts.json",new URL("./",import.meta.url)]]);
   main.builtin("FileAttachment", runtime.fileAttachments(name => fileAttachments.get(name)));
   main.variable(observer()).define(["md"], function(md){return(
 md`# Test`
