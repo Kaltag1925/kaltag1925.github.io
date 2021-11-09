4d5e6304a97e82c23d97c08d363cd4d503167011
Kaltag1925 <pdavisbray@gmail.com>
2021-11-09 14:09:31 -0600

    Polygons

--- test.js
+++ test.js
@@ -1,3 +1,11 @@
-function addone(){
-	console.log(1 + 1);
-}
+var convexHull = require('monotone-convex-hull-2d')
+
+var points = [
+  [0, 0],
+  [1, 0],
+  [0, 1],
+  [1, 1],
+  [0.5, 0.5]
+]
+
+console.log(convexHull(points))
