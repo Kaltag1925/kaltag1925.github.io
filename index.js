20e4e5dc7b504e5a4859a8c6d585b7a0c9bdd8c7
Kaltag1925 <pdavisbray@gmail.com>
2021-11-13 13:21:42 -0600

    a

--- index.js
+++
@@ -1,1 +0,0 @@
-export {default} from "./e98cf432753a4295@791.js";
--- js/map.js
+++ js/map.js
@@ -4,9 +4,43 @@ const width = 1000;
 
 var k = height / width
 
-var artiJson = await d3.json("artifacts.json")
-console.log(artiJson);
-var data = artiJson.UluburunShipwreck.artifact.map(a => a = {location: a.location.map(l => locToInts(l)), description: a.description, _type: a._type, _name: a._name});
+
+function readDataFile() {
+  d3.json("artifacts.json").then(f => startMap(f))
+}
+
+function startMap(dataFile) {
+  loadData(dataFile)
+  setUpMapHelpers()
+  setUpMap()
+  plotFragments()
+  addFragmentLabels()
+  setUpSidebarData()
+  loadState(null)
+}
+
+function loadData(dataFile) {
+  data = dataFile.UluburunShipwreck.artifact.map(a => a = {location: a.location.map((l, index) => assembleFragment(l, a._name, index)), description: a.description, _type: a._type, _name: a._name});
+  var objectsWithALocation = data.map(o => o.location.map(l => ({id: "point" + l.x + "-" + l.y, location: l, object: o}))).flat()
+
+  objs = Array.from(groupBy(objectsWithALocation, o => o.id))
+  objs = objs.map(o => ({id: o[0], location: ({x: o[1][0].location.x, y: o[1][0].location.y}), objects: o[1].map(v => v.object)}))
+
+  var alphaIntDic = d3.range(17).map(i => String.fromCharCode(65 + i)) //dont need this but i was tired mkay?
+}
+
+function assembleFragment(s, objName, index) {
+  var loc = s.split(" ");
+  const y = alphaToInt(loc[0][0])
+  const x = loc[0].slice(1)
+  
+  const id = objName + "-" + "(" + x + ", " + y + ")" + "-" + index;
+  
+  return {id: id, x: x, y:y};
+}
+
+
+
 
 /**
  * @description
@@ -24,32 +58,16 @@ var data = artiJson.UluburunShipwreck.artifact.map(a => a = {location: a.locatio
 function groupBy(list, keyGetter) {
     const map = new Map();
     list.forEach((item) => {
-         const key = keyGetter(item);
-         const collection = map.get(key);
-         if (!collection) {
-             map.set(key, [item]);
-         } else {
-             collection.push(item);
-         }
+          const key = keyGetter(item);
+          const collection = map.get(key);
+          if (!collection) {
+              map.set(key, [item]);
+          } else {
+              collection.push(item);
+          }
     });
     return map;
 }
-
-  var o = data.map(a => a = {head: a.location[0].x + " " + a.location[0].y, others: a})
-  var objs = []
-  var mid = groupBy(o, o => o.head)
-    mid.forEach((v, k) => objs.push({location: {x: k.slice(0,2), y: k.slice(3)}, objects: v.map(x => x.others)})) ////// AAAAAAAAAA
-
-function locToInts(s) {
-  var loc = s.split(" ");
-  console.log(loc[0][0])
-  const y = alphaToInt(loc[0][0])
-  console.log(y)
-  const x = loc[0].slice(1)
-  return {x: x, y:y};
-}
-
-var alphaIntDic = d3.range(17).map(i => String.fromCharCode(65 + i)) //dont need this but i was tired mkay?
 
 function alphaToInt(a) {
   return a.charCodeAt(0) - 65;
@@ -59,134 +77,159 @@ function intToAlpha(n) {
   return String.fromCharCode(n+65);
 }
 
-var y = d3.scaleLinear()
+var y;
+var x;
+
+function setUpMapHelpers() {
+  y = d3.scaleLinear()
     .domain(d3.extent(objs.map(a => a.location), d => d.y)).nice() //hOW TO MAKE EQUAL SIZED TICKS?
     .range([height, 0])
-	
-var x = d3.scaleLinear()
+    
+  x = d3.scaleLinear()
     .domain(d3.extent(objs.map(a => a.location), d => d.x)).nice() // how to LIMIT SCROLL
     .range([0, width])
-	
+}
+    
 function xAxis(g, x) {
     g.attr("transform", `translate(0,${height})`)
     .call(d3.axisTop(x).ticks(12))
     .call(g => g.select(".domain").attr("display", "none"))
-	}
-	
+    }
+    
 function yAxis(g, y) {
     g.call(d3.axisRight(y).ticks(12 * k))
     .call(g => g.select(".domain").attr("display", "none"))
-	}
-	
+    }
+    
 function grid(g, x, y) {
-    g.attr("stroke", "currentColor")
-    .attr("stroke-opacity", 0.1)
-    .call(g => g
-      .selectAll(".x")
-      .data(x.ticks(12))
-      .join(
-        enter => enter.append("line").attr("class", "x").attr("y2", height),
-        update => update,
-        exit => exit.remove()
-      )
-        .attr("x1", d => 0.5 + x(d))
-        .attr("x2", d => 0.5 + x(d)))
-    .call(g => g
-      .selectAll(".y")
-      .data(y.ticks(12 * k))
-      .join(
-        enter => enter.append("line").attr("class", "y").attr("x2", width),
-        update => update,
-        exit => exit.remove()
-      )
-        .attr("y1", d => 0.5 + y(d))
-        .attr("y2", d => 0.5 + y(d)));
-		}
-		
-var margin = ({top: 25, right: 20, bottom: 35, left: 40})
+  g.attr("stroke", "currentColor")
+  .attr("stroke-opacity", 0.1)
+  .call(g => g
+    .selectAll(".x")
+    .data(x.ticks(12))
+    .join(
+      enter => enter.append("line").attr("class", "x").attr("y2", height),
+      update => update,
+      exit => exit.remove()
+    )
+      .attr("x1", d => 0.5 + x(d))
+      .attr("x2", d => 0.5 + x(d)))
+  .call(g => g
+    .selectAll(".y")
+    .data(y.ticks(12 * k))
+    .join(
+      enter => enter.append("line").attr("class", "y").attr("x2", width),
+      update => update,
+      exit => exit.remove()
+    )
+    .attr("y1", d => 0.5 + y(d))
+    .attr("y2", d => 0.5 + y(d)));
+  }
+        
+  var margin = ({top: 25, right: 20, bottom: 35, left: 40})
+  var svg;
+  var gGrid;
+  var gx;
+  var gy;
+  var chart2;
+  var chart;
+  var polygons;
+  var points;
+  var pointsLabels;
+  var lines;
+  var overlap;
+
+  function setUpMap() {
+    svg = d3.select("#map").append("svg")
+    .attr("viewBox", [0, 0, width, height]);
+    
+    gGrid = svg.append("g")
+
+    gx = svg.append("g")
+
+    gy = svg.append("g")
+    
+    chart2 = svg.append("g")
+    chart = chart2.append("g")
+    var image = chart.append("image")
+      .attr("xlink:href", "https://www.superiortrips.com/ShipwreckImages/AlgomaSiteMap.jpg")
+    polygons = chart.append("g")
+    
+    points = chart.append("g")
+    overlap = chart.append("g")
+    lines = chart.append("g")
+    pointsLabels = chart.append("g")
+
+    const zoom = d3.zoom().scaleExtent([0.5, 32])
+      .on("zoom", zoomed)
+
+    function zoomed({transform}) {
+      const zx = transform.rescaleX(x).interpolate(d3.interpolateRound);
+      const zy = transform.rescaleY(y).interpolate(d3.interpolateRound);
+      gx.call(xAxis, zx);
+      gy.call(yAxis, zy);
+      chart.attr("transform", transform).attr("stroke-width", 5 / transform.k);
+      chart.style("stroke-width", 3 / Math.sqrt(transform.k));
+      points.attr("r", 10 / Math.sqrt(transform.k));
+    gGrid.call(grid, zx, zy)
+    };
+            
+    svg.call(zoom)
+      .call(zoom.transform, d3.zoomIdentity)
   
-  const svg = d3.select("#map").append("svg")
-  .attr("viewBox", [0, 0, width, height]);
-  
-
-
-  const gGrid = svg.append("g")
-
-  const gx = svg.append("g")
-
-  const gy = svg.append("g")
-  
-  const chart2 = svg.append("g")
-  const chart = chart2.append("g")
+    Object.assign(svg.node(), {
+      reset() {
+        svg.transition()
+            .duration(750)
+            .call(zoom.transform, d3.zoomIdentity);
+      }
+    });
+  }
 
   // Clipping path for the map, above the main chart so that the clipping doesn't move with the pan and zoom
   
-  chart2.append('defs')
-      	.append('clipPath') // wont plot look at the zoomable scatter plots plotting
-      	.attr('id', 'clip')
-      	.append('rect')
-      		.attr('x', 30)
-         	.attr('y', 0)
-      		.attr('width', width-100)
-      		.attr('height', height-100);
-  chart2.attr("clip-path", "url(#clip)")
+  // chart2.append('defs')
+  //         .append('clipPath') // wont plot look at the zoomable scatter plots plotting
+  //         .attr('id', 'clip')
+  //         .append('rect')
+  //             .attr('x', 30)
+  //            .attr('y', 0)
+  //             .attr('width', width-100)
+  //             .attr('height', height-100);
+  // chart2.attr("clip-path", "url(#clip)")
 
 
   // Basic bg Image
-  chart.append("image")
-  .attr("xlink:href", "http://www.superiortrips.com/ShipwreckImages/AlgomaSiteMap.jpg")
-
 
   let transform;
 
   // Plot the points on the map
-  const points = chart.append("g")
-    .selectAll("points")
-    .data(objs)
-    .join("circle")
-      .attr("cx", d => x(d.location.x))
-     .attr("cy", d => y(d.location.y))
-      .attr("stroke", "green")
-      .attr("data", d => (d))
-      .attr("r", 10)
-      .attr("locations", d => d.location)
-  .style("cursor", "pointer")
-  .style("cursor", "pointer")
-      .on("click", mapIconClicked)
 
-  const pointsLabels = chart.append("g")
-    .selectAll("pointLabelText")
-    .data(objs)
-    .join("text")
-      .attr("x", d => x(d.location.x))
-      .attr("y", d => y(d.location.y))
-      .attr("transform", d => `rotate(-45,${x(d.location.x)},${y(d.location.y)})`)
-      .text(d => getText(d))
-
-  function getText(d) {
-    var text = d.objects[0]._name //HOW DO WE KNOW WHICH ONE IS THE TOP ONE???
-    if (d.objects.length > 1) {
-      text += " +" + (d.objects.length - 1)
-    }
-    return text
+  function plotFragments() {
+    points
+      .selectAll("points")
+      .data(objs)
+      .join("circle")
+        .attr("cx", d => x(d.location.x))
+        .attr("cy", d => y(d.location.y))
+        .attr("stroke", "green")
+        .attr("data", d => (d))
+        .attr("r", 10)
+        .attr("id", d =>  "point" + d.location.x + "-" + d.location.y + "")
+    .style("cursor", "pointer")
+          .attr("visibility", "hidden")
+        .on("click", mapIconClicked)
   }
-            
-  const lines = chart.append("g");
-  const overlap = chart.append("g");
 
-  // When an object dot is clicked
-  function objectClicked(event, p) {
-    overlap.selectAll("*").remove();
-    lines.append("g")
-      .attr("stroke", "red")
-      .attr("stroke-opacity", 0.6)
-      .selectAll("links")
-      .data(p.others.map((frag, index, array) => frag = {x: frag.x, y: frag.y, xNext: array[(index + 1) % array.length].x, yNext: array[(index + 1) % array.length].y}).flat())
-      .join("line")
-        .attr("x1", d => x(d.x))
-        .attr("y1", d => y(d.y))
-        .attr("x2", d => x(d.xNext))
-        .attr("y2", d => y(d.yNext));
+  function addFragmentLabels() {
+      pointsLabels.selectAll("pointLabelText")
+      .data(objs)
+      .join("text")
+        .attr("x", d => x(d.location.x))
+        .attr("y", d => y(d.location.y))
+        .attr("id", d =>  "text" + d.location.x + "-" + d.location.y + "")
+        .attr("visibility", "hidden")
+        .attr("transform", d => `rotate(-45,${x(d.location.x)},${y(d.location.y)})`)
   }
 
   // When a dot on the map is clicked, if there are multiple do that functions
@@ -194,21 +237,24 @@ var margin = ({top: 25, right: 20, bottom: 35, left: 40})
   function mapIconClicked(event, p) {
     overlap.selectAll("*").remove();
     lines.selectAll("*").remove();
-    if (p.objects.length > 1) {
-        multiobjectClicked(event, p)
+    let objectsOnPoint = findObjects(p.id)
+    if (objectsOnPoint.length > 1) {
+        console.log("multi clicked")
+        multiobjectClicked(event, p, objectsOnPoint)
     } else {
-        objectClicked(event, p.objects[0])
+        console.log("object clicked")
+        objectClicked(objectsOnPoint[0])
     }
   }
 
   // When a location with multiple objects is clicked on
-  function multiobjectClicked(event, p) {
-   overlap.append("g")
+  function multiobjectClicked(event, p, objectsOnPoint) {
+    overlap
       .attr("stroke", "steelblue")
       .attr("stroke-width", 1.5)
       .attr("fill", "red")
       .selectAll("overlap")
-    .data(p.objects)
+    .data(objectsOnPoint)
       .join("circle")
       .attr("cx", d => x(p.location.x))
         .attr("cy", (d, i) => y(p.location.y) - i * 10)
@@ -216,44 +262,129 @@ var margin = ({top: 25, right: 20, bottom: 35, left: 40})
         .on("click", objectClicked)
       .style("cursor", "pointer")
   }
+
+
+
+  // When an object dot is clicked
+  function objectClicked(object) {
+    selectObject(object)
+
+    overlap.selectAll("*").remove();
+
+    // const grahamScan = new GrahamScan();
+    // grahamScan.setPoints(object.location.map(l => [parseInt(l.x), l.y]));
+    // const hull = grahamScan.getHull();  // [1,0], [2,1], [0,1]
+
+    var points = object.location.map(l => [parseInt(l.x), l.y])
+    var hull = convexHull(points).map(i => points[i])
+
+    lines.append("g")
+      .attr("stroke", "red")
+      .attr("stroke-opacity", 0.6)
+      .selectAll("links")
+      .data(hull.map((l, index, array) => ({x: l[0], y: l[1], xNext: array[(index + 1) % array.length][0], yNext: array[(index + 1) % array.length][1]})).flat()) //.data(hull.map((l, index, array) => ({x: l[0], y: l[1], xNext: array[(index + 1) % array.length][0], yNext: array[(index + 1) % array.length][1]})).flat())
+      .join("line")
+        .attr("x1", d => x(d.x))
+        .attr("y1", d => y(d.y))
+        .attr("x2", d => x(d.xNext))
+        .attr("y2", d => y(d.yNext));
+
+    
+    // console.log(object.location.map((frag, index, array) => frag = {x: parseInt(frag.x), y: frag.y, xNext: parseInt(array[(index + 1) % array.length].x), yNext: array[(index + 1) % array.length].y}).flat())
+    // console.log(object)
+
+  }
+
+  function shadeObjectRegion(object) {
+    var points = object.location.map(l => [parseInt(l.x), l.y])
+    var hull = convexHull(points).map(i => points[i]).map(p => x(p[0]) + "," + y(p[1])).join(" ")
+
+    chart.selectAll("polygon")
+      .data([hull])
+    .enter().append("polygon")
+      .attr("points", d => d)
+      .attr("fill", "beige"); 
+  }
+
+  function findObjects(id) {
+    return objectsByDots.find(l => l.svgNode.id == id).objects
+  }
   
-  // ID
-  chart.append("g")
-      .attr("font-family", "sans-serif")
-      .attr("font-size", 10)
-    .selectAll("text")
-    .data(data)
-    .join("text")
-      .attr("dy", "0.35em")
-      .attr("x", d => x(d.x) + 7)
-      .attr("y", d => y(d.y))
-      .text(d => d.id);
+  // ID // What does this do?
+  // chart.append("g")
+  //     .attr("font-family", "sans-serif")
+  //     .attr("font-size", 10)
+  //   .selectAll("text")
+  //   .data(data)
+  //   .join("text")
+  //     .attr("dy", "0.35em")
+  //     .attr("x", d => x(d.x) + 7)
+  //     .attr("y", d => y(d.y))
+  //     .text(d => d.id);
 
-const zoom = d3.zoom().scaleExtent([0.5, 32])
-      .on("zoom", zoomed)
-
-function zoomed({transform}) {
-  console.log(transform)
-    const zx = transform.rescaleX(x).interpolate(d3.interpolateRound);
-    const zy = transform.rescaleY(y).interpolate(d3.interpolateRound);
-    gx.call(xAxis, zx);
-   gy.call(yAxis, zy);
-    chart.attr("transform", transform).attr("stroke-width", 5 / transform.k);
-    chart.style("stroke-width", 3 / Math.sqrt(transform.k));
-    points.attr("r", 10 / Math.sqrt(transform.k));
-  gGrid.call(grid, zx, zy)
-  };
-          
-  svg.call(zoom)
-    .call(zoom.transform, d3.zoomIdentity)
-
-  Object.assign(svg.node(), {
-    reset() {
-      svg.transition()
-          .duration(750)
-          .call(zoom.transform, d3.zoomIdentity);
-    }
-  });
+  function objectToNode(object) {
+    var objectName = object._name;
+    var locations = object.location;
+    var mainNode = {id: objectName, text: objectName, count: locations.length, node: locations.map((f, i) => fragmentToNode(f, i, objectName)) };
+    return mainNode;
+  }
   
-  //w2ui
+  function fragmentToNode(fragment, index, objectName) {
+        var fragmentName = "(" + fragment.x + ", " + fragment.y + ")";
+        return { id: objectName + "-" + fragmentName + "-" + index + "-button", text: fragmentName, onClick: function(event) {
+            
+        d3.select("#" + "point" + fragment.x + "-" + fragment.y + "").attr("r", 100)
+        }}
+  }
+  
+  function setUpSidebarData() {
+    var nodes = data.map(objectToNode);
+    w2ui['navigation'].add(nodes);
 
+  /* Var setting */
+    objectsByDots = objs.map(e => ({svgNode: document.getElementById("point" + e.location.x + "-" + e.location.y),
+                                  textNode: document.getElementById("text" + e.location.x + "-" + e.location.y),
+                                  location:e.location, 
+                                  objects: []}))
+  }
+
+  // Divider
+  
+  /*
+  console.log(data)
+    data.forEach((object) => {
+        var objectName = object._name;
+        var locations = object.location;
+        w2ui['navigation'].add({ id: objectName, text: objectName });
+        locations.forEach((fragment, index) => {
+            var fragmentName = "(" + fragment.x + ", " + fragment.y + ")";
+            w2ui['navigation'].add(objectName, ({ id: objectName + "-" + fragmentName + "-" + index + "-button", text: fragmentName, onClick: function(event) {
+            
+            d3.select("#" + "point" + fragment.x + "-" + fragment.y + "").attr("r", 100)
+            }}));
+            
+            // this takes a bit maybe we should pregen it
+            
+            
+        });
+    });
+    */
+    
+/*
+import define from "./index.js";
+import {Runtime, Library, Inspector} from "./runtime.js";
+const runtime = new Runtime();
+    // Inspector.into(document.body)
+const main = runtime.module(define, Inspector.into(document.getElementById("map")));
+// const main = runtime.module(define, name => {
+// 	console.log(name);
+// 	if (name == 'replay1') {
+// 		console.log(define);
+// 		console.log("Query");
+// 		console.log(document.querySelection("#map"));
+// 		console.log("ID");
+// 		console.log(document.getElementById("map"));
+// 		return new Inspector.into(document.body);
+// 	}
+// });
+*/
